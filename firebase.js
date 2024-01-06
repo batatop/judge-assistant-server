150457885b779ab8c17eafca75f139d7a854329c
batatop <batatop@gmail.com>
2024-01-07 00:46:06 +0300

    added firebase admin

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://judge-assistant-3c8dd-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "judge-assistant-3c8dd.appspot.com" 
+});
+
+exports.storage = admin.storage();
